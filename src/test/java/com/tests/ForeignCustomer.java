66cb4e8314404c43d03d90d43941215706a570dc
Jeyhun Ali <Ali-ceyhun@list.ru>
2019-09-26 17:34:23 -0400

    new features added and modified some file

---
+++ src/test/java/com/tests/ForeignCustomer.java
@@ -0,0 +1,9 @@
+package com.tests;
+
+public class ForeignCustomer {
+
+	public static void main(String[] args) {
+// this class purpose just to see hoe to add this class to git repo 
+	}
+
+}
--- src/test/java/com/tests/Tc_edit_customer.java
+++ src/test/java/com/tests/Tc_edit_customer.java
@@ -1,2 +1,6 @@
 //this is the new test cases 
 //in test cases we will add some new customer 
+ 
+
+//this is the new code addedd 
+// purpose of git 
